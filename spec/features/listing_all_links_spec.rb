1a979f2ee02ed8b06ab090f0deb77731b1c6be61
JamesGraham <jamesgraham09@gmail.com>
2013-12-18 17:23:06 +0000

    added tag testing functionality

--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -3,11 +3,29 @@ require 'spec_helper.rb'
 feature "user browses the list of links" do
 	before (:each) {
 		Link.create(:url => "http://www.makersacademy.com",
-					:title => "Makers Academy")
+					:title => "Makers Academy",
+					:tags => [Tag.first_or_create(:text => 'education')])
+		Link.create(:url => "http://www.google.com",
+					:title => "Google",
+					:tags => [Tag.first_or_create(:text => 'search')])
+		Link.create(:url => "http://www.bing.com",
+					:title => "Bing",
+					:tags => [Tag.first_or_create(:text => 'search')])
+		Link.create(:url => "http://codewars.com",
+					:title => "Code Wars",
+					:tags => [Tag.first_or_create(:text => 'education')])
 		}
 
 		scenario "when opening the home page" do
 			visit "/"
 			expect(page).to have_content("Makers Academy")
+		end
+
+		scenario "filtered by tag" do
+			visit 'tags/search'
+			expect(page).not_to have_content("Makers Academy")
+			expect(page).not_to have_content("codewars.com")
+			expect(page).to have_content("Google")
+			expect(page).to have_content("Bing")
 		end
 end
