e4f8a8b97b606c832894cb08a7f0eb38a93b7e55
JamesGraham <jamesgraham09@gmail.com>
2013-12-20 13:19:22 +0000

    refactored layout (the long way)

---
+++ app/lib/link.rb
@@ -0,0 +1,10 @@
+#link class corresponds to a table in the database
+class Link
+	#makes instances of the link class datamapper resources
+	#then describe the resources out model will have
+	include DataMapper::Resource
+	has n, :tags, :through => Resource
+	property :id, Serial
+	property :title, String
+	property :url, String
+end
--- app/server.rb
+++ app/server.rb
@@ -8,69 +8,15 @@ require './lib/tag'
 
 require_relative 'data_mapper_setup'
 require_relative 'helpers/application'
+require_relative 'controllers/application'
+require_relative 'controllers/sessions'
+require_relative 'controllers/links'
+require_relative 'controllers/tags'
+require_relative 'controllers/users'
 
 set :root, File.dirname(__FILE__)
 
 enable :sessions
 set :session_secret,"test"
 use Rack::Flash
-
-get '/' do
-	@links = Link.all
-	erb :index
-end
-
-post '/links' do
-	url = params["url"]
-	title = params["title"]
-	tags = params["tags"].split(" ").map do |tag|
-		Tag.first_or_create(:text => tag)
-	end
-	Link.create(:url => url, :title => title, :tags => tags)
-	redirect to('/')
-end
-
-get '/tags/:text' do
-	tag = Tag.first(:text => params[:text])
-	@links = tag ? tag.links : []
-	erb :index
-end
-
-post '/users' do
-	@user = User.create(:email => params[:email],
-					:password => params[:password],
-					:password_confirmation => params[:password_confirmation])
-	if @user.save
-		session[:user_id] = @user.id
-		redirect to('/')
-	else
-		flash.now[:errors] = @user.errors.full_messages
-		erb :"users/new"
-	end
-end
-
-get '/users/new' do
-	@user = User.new
-	erb :"users/new"
-end
-
-get '/sessions/new' do
-	erb :"sessions/new"
-end
-
-post "/sessions" do
-	email, password = params[:email], params[:password]
-	user = User.authenticate(email, password)
-	if user
-		session[:user_id] = user.id
-		redirect to('/')
-	else
-		flash[:errors] = ["The email or password are incorrect"]
-		erb :"sessions/new"
-	end
-end
-
-delete '/sessions' do
-	"Good bye!"
-end
 
